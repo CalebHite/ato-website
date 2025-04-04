eba2064978a4a026e111231f640c695940dbb9c3
Charlie <charlieedoherty@gmail.com>
2025-04-04 00:41:52 -0500

    Add Parents/Alumni Page

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -14,7 +14,7 @@ const Navbar = ({ theme }) => {
     { id: 0, label: 'Home', sectionId: '' },
     { id: 1, label: 'About', sectionId: 'about' },
     { id: 2, label: 'Recruitment', sectionId: 'recruitment' },
-    { id: 3, label: 'Scholarship', sectionId: 'scholarship' },
+    { id: 3, label: 'Parents & Alumni', sectionId: 'parents-alumni' },
   ];
 
   const handleTabClick = (id, sectionId) => {
@@ -35,20 +35,20 @@ const Navbar = ({ theme }) => {
   const borderColor = theme === 'dark' ? 'border-gray-300' : 'border-gray-800';
 
   return (
-    <nav className="w-full backdrop-blur-sm shadow-md z-50 fixed top-0 border-b border-transparent p-6 bg-transparent">
+    <nav className="w-full backdrop-blur-sm shadow-md z-50 fixed top-0 border-b border-transparent bg-transparent">
       <div className="max-w-7xl mx-auto px-4 flex justify-between items-center">
         <img
           src="images/gold-cross.png"
           alt="Gold Cross Left"
           className="w-18 h-18"
         />
-        <div className="max-w-[32rem] flex justify-center">
+        <div className="max-w-[40rem] flex justify-center">
           <div className="relative flex space-x-2 overflow-hidden rounded-full p-1.5">
             {tabs.map((tab) => (
               <button
                 key={tab.id}
                 onClick={() => handleTabClick(tab.id, tab.sectionId)}
-                className={`cursor-pointer hover:text-orange-300 ${textColor} font-bold border border-2 ${borderColor} relative z-10 flex w-full items-center justify-center px-3 py-2 text-md font-medium transition-colors duration-300 rounded-full ${montserrat.className}
+                className={`cursor-pointer hover:text-orange-300 ${textColor} font-bold border border-2 ${borderColor} relative z-10 flex w-full items-center justify-center px-3 py-2 text-lg whitespace-nowrap font-medium transition-colors duration-300 rounded-full ${montserrat.className}
                   ${activeTab === tab.id ? borderColor : 'border-transparent'}`}
               >
                 {tab.label}
