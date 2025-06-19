b210597563cc7e3455b8110d7c46e270dbb654ca
Charlie Doherty <t53w5cw@homedepot.com>
2025-06-19 13:36:19 -0400

    Change Description

--- src/app/metadata.js
+++ src/app/metadata.js
@@ -1,6 +1,6 @@
 export const metadata = {
   title: "ATΩ Kansas",
-  description: "University of Kansas chapter of the Alpha Tau Omega Fraternity",
+  description: "University of Kansas chapter of the Alpha Tau Omega Fraternity. ATO Gamma Mu",
   icons: {
     icon: "/images/gold-cross.png",
   },
